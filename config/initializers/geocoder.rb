40ba3e87ff9df9c2e0fd1f8ebe78d9d39452216a
Romain BERNARD <romain.bernard@edhec.com>
2015-06-30 18:30:10 +0200

    added picture to homepage and search bar with autocomplete

---
+++ config/initializers/geocoder.rb
@@ -0,0 +1,8 @@
+# config/initializers/geocoder.rb
+
+Geocoder.configure(
+  lookup:    :google,
+  api_key:   ENV['GOOGLE_API_KEY'],
+  use_https: true,
+  units:     :km       # :km for kilometers or :mi for mile
+)
