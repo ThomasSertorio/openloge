a77be47aeeae60580c12cb96a5432a9fdb1616c6
Thomas Sertorio <thomas.sertorio@gmail.com>
2015-07-08 19:00:50 +0200

    status but still work

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -17,6 +17,19 @@ class BookingPolicy < ApplicationPolicy
     record.service.user == user # Only the concerned expert can update the booking
   end
 
+  def accept?
+    record.user == user # Only the concerned member can validate
+  end
+
+  def refuse?
+    record.user == user # Only the concerned member can validate
+  end
+
+  def mission_done?
+    record.service.user == user # Only the concerned expert can update the booking
+  end
+
+
   # def destroy?
   #   false  # No one should be able to update a loge but the create route does not exist so far
   # # end
