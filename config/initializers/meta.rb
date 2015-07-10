ba297413e0b8da196fae19d95ee8b4aa369a5e46
Thomas Sertorio <thomas.sertorio@gmail.com>
2015-07-10 11:29:54 +0200

    meta 2

--- config/initializers/meta.rb
+++
@@ -1,4 +0,0 @@
-default_title: "OpenLoge | Services Locaux Durables"
-meta_description: "Retrouvez l'équipe d'experts de votre quartier"
-meta_image: "OpenLoge_Concept.png" # Une image dans votre dossier app/assets/images/
-twitter_account: "@OpenLoge" #indispensable pour les Twitter Cards
